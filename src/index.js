f13a87cc728a8613753027b21ab4cb224636882d
delcidj22 <jdelcdid23@gmail.com>
2022-01-22 22:38:32 -0800

    Copy files to new repo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import './css/styles.css'; 
+import CurrencyExchange from "./js/exchange-rate-api";
+
+function clearFields() {
+  $('#amount').val("");
+  $('.showErrors').text("");
+  $('.showCurrencyExchange').text("");
+  $('.showConversion').text("");
+}
+
+$(document).ready(function() {
+  $('#currencyConverter').click(function() {
+    event.preventDefault;
+    let country = $('.countryOptions').val();
+    let amount = $('#amount').val(); 
+    clearFields();
+    let promise = CurrencyExchange.getExchange(country, amount);
+    promise.then(function(response) {
+      const body = JSON.parse(response); 
+      $('.showCurrencyExchange').text(`$${amount} in ${country} will exchange to: ${body.conversion_result}`);
+      $('.showConversion').text(`The current conversion rate is: ${body.conversion_rate}`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error: ${error}`);
+    });
+  });
+});
+
